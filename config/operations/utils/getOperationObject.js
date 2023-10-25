68962229018368cdd167af5349761104dcffebb6
mar4enkom <mar44enkom@gmail.com>
2023-10-25 12:02:39 +0300

    fixed negative numbers in start of expression bug

--- config/operations/utils/getOperationObject.js
+++ config/operations/utils/getOperationObject.js
@@ -3,18 +3,9 @@ import {Regex} from "../../../constants/regex.js";
 import {Symbols} from "../../../constants/constants.js";
 import {signs} from "../configs/signsConfig.js";
 
-const getOperatorRegexSource = (regexSource) =>
-    `${Regex.NUMBER.source}${regexSource}${Regex.NUMBER.source}`
-
-const getOperationRegexSource = (operationSignRegexSource) => ({
-    [Operations.CONSTANT]: `${operationSignRegexSource}`,
-    [Operations.SIGN]: `${Regex.NUMBER.source}${operationSignRegexSource}`,
-    [Operations.OPERATOR_LOW_PRIORITY]: getOperatorRegexSource(operationSignRegexSource),
-    [Operations.OPERATOR_HIGH_PRIORITY]: getOperatorRegexSource(operationSignRegexSource),
-    [Operations.FUNCTION]: `${operationSignRegexSource}${Regex.MOST_NESTED_PARENTHESES.source}`
-});
-
-const operatorExtractor = (sign, expression) => expression.split(sign);
+const operatorExtractor = (sign, expression) => {
+    return expression.split(sign);
+};
 
 const OperationArgsExtractors = {
     [Operations.CONSTANT]: (sign, expression) => [sign],
@@ -27,7 +18,7 @@ const OperationArgsExtractors = {
     }
 }
 
-const getOperationSignRegex = (operationsObj) => {
+const getOperationsSignRangeRegex = (operationsObj) => {
     const signSymbols = Object.keys(operationsObj);
     const signSymbolsRegexStr = signSymbols
         .map(s => Regex.REGEX_RESERVED_SYMBOL.test(s) ? `\\${s}` : s)
@@ -35,13 +26,37 @@ const getOperationSignRegex = (operationsObj) => {
     return new RegExp(`(${signSymbolsRegexStr})`);
 }
 
-const getExtractOperationSignFunc = (operationsObj) => (expression) => {
-    const operationSignRegex = getOperationSignRegex(operationsObj);
+const getExtractOperationSignFunc = (operationsObj, operationCategory) => (expression) => {
+    const getOperatorSignRegexSource = (regexSource) =>
+        `(?<=\\d)${regexSource}(?=${Regex.NUMBER.source})`
+
+    const getOperationSignRegex = (operationSignRegexSource) => ({
+        [Operations.CONSTANT]: `${operationSignRegexSource}`,
+        [Operations.SIGN]: `${operationSignRegexSource}`,
+        [Operations.OPERATOR_LOW_PRIORITY]: getOperatorSignRegexSource(operationSignRegexSource),
+        [Operations.OPERATOR_HIGH_PRIORITY]: getOperatorSignRegexSource(operationSignRegexSource),
+        [Operations.FUNCTION]: `${operationSignRegexSource}`
+    });
+
+    const operationsRangeSignRegexSource = getOperationsSignRangeRegex(operationsObj).source;
+    const operationSignRegexSource = getOperationSignRegex(operationsRangeSignRegexSource)[operationCategory];
+    const operationSignRegex = new RegExp(operationSignRegexSource);
     return operationSignRegex.exec(expression)?.[0];
 }
 
 const getExtractOperationBodyFunc = (operationsObj, operationCategory) => (expression) => {
-    const operationSignRegex = getOperationSignRegex(operationsObj);
+    const getOperatorRegexSource = (regexSource) =>
+        `${Regex.NUMBER.source}${regexSource}${Regex.NUMBER.source}`
+
+    const getOperationRegexSource = (operationSignRegexSource) => ({
+        [Operations.CONSTANT]: `${operationSignRegexSource}`,
+        [Operations.SIGN]: `${Regex.NUMBER.source}${operationSignRegexSource}`,
+        [Operations.OPERATOR_LOW_PRIORITY]: getOperatorRegexSource(operationSignRegexSource),
+        [Operations.OPERATOR_HIGH_PRIORITY]: getOperatorRegexSource(operationSignRegexSource),
+        [Operations.FUNCTION]: `${operationSignRegexSource}${Regex.MOST_NESTED_PARENTHESES.source}`
+    });
+
+    const operationSignRegex = getOperationsSignRangeRegex(operationsObj);
     const operationRegexSource = getOperationRegexSource(operationSignRegex.source)[operationCategory];
 
     const operationRegex = new RegExp(operationRegexSource);
@@ -54,17 +69,17 @@ export function getOperationObject(operationsList, operationCategory) {
         return { ...acc, [props.sign]: props};
     }, {});
 
-    const extractOperationSign = getExtractOperationSignFunc(newOperationsObj);
-    const extractOperands = OperationArgsExtractors[operationCategory];
     const extractOperationBody = getExtractOperationBodyFunc(newOperationsObj, operationCategory);
+    const extractOperationSign = getExtractOperationSignFunc(newOperationsObj, operationCategory);
+    const extractOperands = OperationArgsExtractors[operationCategory];
 
     return {
         [operationCategory]: {
             priority: OperationByPriority[operationCategory],
             operations: newOperationsObj,
+            extractOperationBody,
             extractOperationSign,
             extractOperands,
-            extractOperationBody,
         }
     }
 }
