a741e07cd610bb718dd16a7e0e5551ce7196309f
mar4enkom <mar44enkom@gmail.com>
2023-11-14 08:47:29 +0200

    improved validateParenthesesNesting tests

--- src/modules/calculator/controller/utils/validateParenthesesNesting/validateParenthesesNesting.test.js
+++ src/modules/calculator/controller/utils/validateParenthesesNesting/validateParenthesesNesting.test.js
@@ -1,19 +1,43 @@
 import {validateParenthesesNesting} from "./validateParenthesesNesting.js";
 
 describe('expression parentheses nesting validation', () => {
+    test("Empty expression", () => {
+        expect(validateParenthesesNesting("")).toBe(true);
+    });
+
+    test("Expression without parentheses", () => {
+        expect(validateParenthesesNesting("2+3")).toBe(true);
+    });
+
+    test("Non-closed parentheses", () => {
+        expect(validateParenthesesNesting("(")).toBe(false);
+    });
+
     test("Empty parentheses", () => {
         expect(validateParenthesesNesting("()")).toBe(false);
     });
 
     test("Incorrect parentheses order", () => {
-        expect(validateParenthesesNesting(")(")).toBe(false);
+        expect(validateParenthesesNesting(")2(")).toBe(false);
     });
 
     test("Outer expression nesting", () => {
         expect(validateParenthesesNesting("(2+(2+(2)+(2)))")).toBe(true);
     });
 
-    test("2+(2+(2)+(2))", () => {
+    test("Correct expression", () => {
         expect(validateParenthesesNesting("2+(2+(2)+(2))")).toBe(true);
+    });
+
+    test("Extra right parentheses", () => {
+        expect(validateParenthesesNesting("2+(2+(2)+(2)))")).toBe(false);
+    });
+
+    test("Empty parentheses inside expression", () => {
+        expect(validateParenthesesNesting("2+(2+(2)+()+(2)))")).toBe(false);
+    });
+
+    test("Specific symbols in parentheses", () => {
+        expect(validateParenthesesNesting("2+(2^2!.,$^)")).toBe(true);
     });
 });
