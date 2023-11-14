0be1d4bb83672012e5b09eda174b15d945163f55
mar4enkom <mar44enkom@gmail.com>
2023-11-14 08:34:57 +0200

    fixed aliases for jest

--- jest.config.js
+++ jest.config.js
@@ -3,6 +3,9 @@ const config = {
     testMatch: ['**/*.js'],
     testPathIgnorePatterns: ['/node_modules/'],
     moduleFileExtensions: ['js'],
+    "moduleNameMapper": {
+        "^UserConfig(.*)$": "<rootDir>/userConfig/$1",
+    }
 };
 
 export default config;
