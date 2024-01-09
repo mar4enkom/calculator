32cc5ce6c4b11f61c75351202eb4dbbe94fd2d7f
mar4enkom <mar44enkom@gmail.com>
2024-01-09 11:59:21 +0200

    client: developed restarting process and sending report to logger if unaxpected error or unhandled rejection

--- packages/server/src/shared/utils/processHandlers.ts
+++ packages/server/src/shared/utils/processHandlers.ts
@@ -1,11 +1,28 @@
 import ErrorHandler from "../errors/ErrorHandler";
+import {ServerError} from "../errors/ServerError";
+import {HttpStatusCodes} from "../constants/httpStatusCodes";
+import {ServerErrorCodes} from "../constants/serverErrors";
 
 export const handleUncaughtException = async (error: Error): Promise<void> => {
     console.error("Uncaught exception");
-    ErrorHandler.handleError(error);
+    const serverError = new ServerError(
+        HttpStatusCodes.INTERNAL_SERVER_ERROR,
+        ServerErrorCodes.UNKNOWN_SERVER_ERROR,
+        "Uncaught exception",
+        true
+    );
+    await ErrorHandler.handleError(serverError);
+    process.exit(0);
 };
 
 export const handleUnhandledRejection = async (reason: Error): Promise<void> => {
     console.error("Unhandled rejection");
-    ErrorHandler.handleError(reason);
+    const serverError = new ServerError(
+        HttpStatusCodes.INTERNAL_SERVER_ERROR,
+        ServerErrorCodes.UNKNOWN_SERVER_ERROR,
+        "Unhandled rejection",
+        true
+    );
+    ErrorHandler.handleError(serverError);
+    process.exit(0);
 };
